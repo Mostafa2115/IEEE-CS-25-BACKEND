f5c60e7dacb791ed876e328b42db734251356908
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-02-14 17:26:40 +0200

    .

--- MySQL/Task_4.sql
+++ MySQL/Task_4.sql
@@ -5,4 +5,13 @@ CASE WHEN A + B > C AND A + C > B AND B + C > A THEN
 ELSE 'Not A Triangle'END
 FROM TRIANGLES;
 
--- problem two (
+-- problem five (Query the average population for all cities in CITY, rounded down to the nearest integer)
+select floor(avg(POPULATION))
+from CITY;
+
+-- problem seven (Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.Write a query to help Eve)
+SELECT CASE WHEN G.Grade < 8 THEN 'NULL' ELSE S.Name END, G.Grade, S.Marks
+FROM Students S
+JOIN Grades G ON S.Marks BETWEEN G.Min_Mark AND G.Max_Mark
+ORDER BY G.Grade DESC, S.Name;
+
