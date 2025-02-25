f9b049a8b5e532e2bc866a46e32d5abf67b7799a
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-02-25 21:55:41 +0200

    Update task4.php

--- PHP/Task_1/Assignment-3/task4.php
+++ PHP/Task_1/Assignment-3/task4.php
@@ -1,7 +1,9 @@
 <? php
-  $a = 200;
-$b = &$a;
-$a = 100;
-
-echo $b; 
-?>
+  echo $_SERVER['SERVER_ROOT'];
+echo <br>;
+  echo $_SERVER['SERVER_NAME'];
+echo <br>;
+echo getenv('systemroot');
+echo <br>;
+echo getenv('OPENSSL_CONF');
+  ?>
