a88dcf4d573f66c2d2b3eb7e13feb5b9b2ddd596
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-02-27 00:16:22 +0200

    Create task4.php

---
+++ PHP/Task_2/Assignment-2/task4.php
@@ -0,0 +1,16 @@
+<? php
+$start = 10;
+$end = 0;
+$stop = 3;
+for ($i=$start;$i>=$end;$i--)
+{
+    if ($i<10){
+      echo "0";
+    }
+    echo "$i<br>";
+    if($i==3)
+    {
+      break;
+    }
+}
+?>
