b80f23091190c7819b3b09fda2acbb46d488c778
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-02-27 00:24:19 +0200

    Update task4.php

--- PHP/Task_2/ِAssignment-1/task4.php
+++ PHP/Task_2/ِAssignment-1/task4.php
@@ -2,7 +2,7 @@
     $a = 30;
     $b = 20;
     $c = 10;
-    echo ($a + $b === $c)?"A + B = C" : ($a + $c === $b)? "A + C = B":($b + $c === $a)?"B + C = A":"The End";
+    echo ($a + $b === $c)? "A + B = C" : (($a + $c === $b)? "A + C = B":(($b + $c === $a)?"B + C = A":"The End"));
     
     
     
