0cac2cd906051564bed85675c3e52a78cfe9f5a6
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-02-26 23:33:32 +0200

    Create task2.php

---
+++ PHP/Task_2/Assignment-2/task2.php
@@ -0,0 +1,24 @@
+<? php
+$index=0;
+while($index<=20)
+{
+  if($index%2==0){
+    echo $index;
+    echo "<br>";
+  }
+  $index++;
+}
+$index=0;
+do{
+  if($index%2==0){
+    echo $index;
+    echo "<br>";
+  }
+  $index++;  
+}while($index<=20)
+
+for ($index=1;$index<=20;$index++)
+{
+  echo "$index<br>";
+}
+?>
