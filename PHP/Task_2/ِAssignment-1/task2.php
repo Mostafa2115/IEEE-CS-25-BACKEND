70adf505aa1ace3cfcffd1e84f45f7558c53fc26
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-02-26 22:39:41 +0200

    Create task2.php

---
+++ PHP/Task_2/ِAssignment-1/task2.php
@@ -0,0 +1,24 @@
+<? php
+  // Test Case 1
+$a = 100;
+$b = 200;
+$c = 300;
+if ($a<>$b or $a<>$c)
+{echo "A Is Not Larger Than B Or C";}
+echo "<br>";
+
+// Test Case 2
+$a = 200;
+$b = 100;
+$c = 300;
+if ($a>$b)
+{echo "A Is Larger Than B";}
+echo "<br>"
+
+// Test Case 3
+$a = 200;
+$b = 200;
+$c = 100;
+if ($a>$c)
+{echo "A Is Larger Than c";}
+?>
