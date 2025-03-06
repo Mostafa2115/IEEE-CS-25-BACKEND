0b11cb4bafbca29f1a4e08eb931ad0022f655055
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-03-06 13:52:23 +0200

    Create task1.php

---
+++ PHP/Task_4/Assignment-2/task1.php
@@ -0,0 +1,6 @@
+<?php
+date_default_timezone_set("Asia/Riyadh");
+echo date_default_timezone_get() . PHP_EOL;
+echo date("D, d M y - h:i:s A") . PHP_EOL;
+echo date("l, d F Y - h:i:s A") . PHP_EOL;
+?>
